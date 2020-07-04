29fef72260d325bbc7bb2417632054c6505f8b6a
IndianCoderMayank <manudewaker@gmail.com>
2020-07-04 20:10:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+ function myfun(){
+document.getElementById('wrapper').className="animate";
+document.getElementById('close-btn').style.display = 'flex';
+
+ setTimeout( function myfun(){
+document.getElementById('main').style.display = 'none';
+document.getElementById('header').style.display = 'block';
+document.getElementById('header').className = 'head_animate';
+},2500
+);
+
+}
+
+
+
+function myfun2(){
+document.getElementById('wrapper').className="wrapper";
+}
+	
